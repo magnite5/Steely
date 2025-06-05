c0c14dbc6f853885c020f50c833431b88c8044ac
magnite5 <87040030+magnite5@users.noreply.github.com>
2025-06-04 20:50:54 -0400

    Initial commit

---
+++ src/main/java/com/magnoix/steely/Steely.java
@@ -0,0 +1,16 @@
+package com.magnoix.steely;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Steely implements ModInitializer {
+	public static final String MOD_ID = "steely";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/magnoix/steely/SteelyClient.java
@@ -0,0 +1,10 @@
+package com.magnoix.steely;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SteelyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
